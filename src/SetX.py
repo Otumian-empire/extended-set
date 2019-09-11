991d82dcaa65b56b030496603cab3bebdcc27809
otumian <popecan1000@gmail.com>
2019-09-11 13:30:51 +0000

    src/SetX.py - added the contructor method and the __repr__ method

--- src/SetX.py
+++ src/SetX.py
@@ -1,1 +1,9 @@
 class SetX(set):
+    """ An extension of the set class with additional methods for set cardinality, cartesian product, power set, etc """
+
+    def __init__(self, iterable=[]):
+        super().__init__()
+        self.iterable = set(iterable)
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__.upper()!a}> {self.iterable}"
